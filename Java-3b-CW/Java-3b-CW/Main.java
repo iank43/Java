72f745e489cd78c98b7b311539a6a152cfdbccf8
iank43 <iank43@nycstudents.net>
2025-09-11 16:34:16 +0000

    ....

---
+++ Java-3b-CW/Java-3b-CW/Main.java
@@ -0,0 +1,33 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  // This is a single line comment in Java
+  // Multi-line comments are enclosed within  /*  and  */
+  
+  /* 
+  Init function which runs when 
+  the program begins is invoked(called) from within the Main function.
+  */
+  void init(){
+    // print Welcome
+    System.out.println("Welcome")
+
+    // print Hello with the quotes displayed
+    System.out.println(/"Hello")
+    
+    // print \Java Code\ as shown
+    System.out.println("//Java Code//")
+    
+    // print the name Joe tabbed once
+    System.out.println("/t joe")
+
+    // print the name Mary tabbed twice
+    
+    
+    // print one, two and three on separate lines using a single line of code
+      
+	
+  }
+  
+}
