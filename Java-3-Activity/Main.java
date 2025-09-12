8e0942ab9a0093b634298dfc03a32434d9cd0a9c
iank43 <iank43@nycstudents.net>
2025-09-12 16:00:23 +0000

    ..

---
+++ Java-3-Activity/Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+  void init(){
+   System.out.println("Hi");
+  
+   System.out.println("Ian Kim");
+
+
+
+  }
+
+  
+}
