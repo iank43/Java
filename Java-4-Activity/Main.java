cb8c54efce77158f6da92045d1f2879cee202525
iank43 <iank43@nycstudents.net>
2025-09-17 16:32:20 +0000

    .

---
+++ Java-4-Activity/Main.java
@@ -0,0 +1,58 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+ void init(){
+// Define a variable for each situation in the challenges below. Assign a value to each variable.
+// Display the value of each variable to the console.
+
+/*  Challenge 1:    
+    1) student's age 
+    2) number of people that live with you
+    3) number of items in a cart 
+*/
+   int student_age = 17;
+   System.out.println(student_age);
+   System.out.println("Student's age is "+ student_age);
+   int ppl_live = 2;
+   System.out.println(ppl_live);
+   System.out.println("I live with "+ ppl_live + " people");
+   int NumOfItems = 12;
+   System.out.println(NumOfItems);
+   System.out.println("The number of items in cart is " + NumOfItems);
+/*  Challenge 2:
+    1) temperature 
+    2) student's GPA
+    3) growth rate of your investment
+*/
+    double temp = 97.3;
+    System.out.println("Your temperature is " + temp);
+    double gpa = 3.8;
+    System.out.println("Your gpa is " + gpa);
+    double invstmnt = 5000.12;
+    System.out.println("growth rate of investment is " + invstmnt);
+/*  Challenge 3:
+    1) Student's last name 
+    2) student's first initial
+    3) description of an item 
+*/
+    String last_name = "Kim";
+    System.out.println("Students last name is " + last_name);
+    String F_initial = "I";
+    System.out.println("Students first name initial is " + F_initial);
+    String desc = "squishy";
+    System.out.println("The description of the item says " + desc);
+/*  Challenge 4:
+    1) the status whether lights are on
+    2) the status whether the game is over
+    3) the status whether the battery is charging
+*/
+
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
